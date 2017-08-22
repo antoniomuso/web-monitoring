a38421905030f54e743226d3f924487be0b0ccb6
AntonioMusolino <antoniomusolino007@gmail.com>
2017-08-22 17:16:33 +0200

    add active intervall

--- bin/web_monitoring.js
+++ bin/web_monitoring.js
@@ -2,11 +2,11 @@ const WebPage = require('./web_page.js')
 
 module.exports.monitor = function (url, options) {
     if (options instanceof Object || 
-        (typeof options === 'function' && options.arguments.length >= 2) ) {
+        (typeof options === 'function') ) {
         return new WebPage(webPage, options)
     } 
     var wp = new WebPage(url, options)
-    process.nextTick(() => wp.emit('error', new Error('wrong arguments of monitor function')))
+    process.nextTick(() => wp.emit('error', new TypeError('wrong arguments of monitor function')))
     wp.close()
     return wp
 }
--- bin/web_page.js
+++ bin/web_page.js
@@ -1,43 +1,63 @@
 const EventEmitter = require('events')
-const request = require('request')
+const util = require('util')
+const request = util.promisify(require('request'))
+
 
 const lapseDefault = 10000
-const whileControlDefault = (oldPage,newPage) => oldPage === newPage
-
-class WebPage extends EventEmitter {
-    constructor(webPage, options) {
-        super()
-        this.active = false
-        this.control = (options.whileControl && typeof options.whileControl === 'function' )
-            ? options.whileControl 
-            : whileControlDefault
-        this.lapse = (options.lapse && typeof options.lapse === 'number') 
-            ? options.lapse
-            : lapseDefault
-        
-        
+const whileControlDefault = (oldPage, newPage) => oldPage === newPage
 
 
+var call = function () { 
 
+    function doRequest(url, webPage) {
+        request(url,(error, response, body) => {
+            if (error) return process.nextTick(() => webPage.emit('error', error))
+            
+        })
+    }
 
+    function activeInterval (wp, control, lapse) {
+        setInterval( () => {
 
+        }, lapse)
+    }
 
+    class WebPage extends EventEmitter {
+        constructor(url, options) {
+            super()
+            this.active = false
+            this.control = (options.whileControl && typeof options.whileControl === 'function')
+                ? options.whileControl
+                : whileControlDefault
+            this.lapse = (options.lapse && typeof options.lapse === 'number')
+                ? options.lapse
+                : lapseDefault
+            this.numberInterval = undefined
+            if (typeof options === 'function') this.control = options
+            this.page = undefined
+
+        }
+
+        start() {
+            
+            request(url).then( (error,responce,body) => {
+                if (error) return process.nextTick(() => this.emit('error',error))
+                this.page = body
+                
+            })
+
+        }
+
+        close() {
+
+        }
+
+        stop() {
+
+        }
 
 
     }
-
-    start () {
-        
-    }
-
-    close () {
-
-    }
-
-    stop () {
-
-    }
-
-
-}
+    return WebPage
+} ()
 module.exports = WebPage
