7e07c299806701d6f464878e817603ab9be53617
AntonioMusolino <antoniomusolino007@gmail.com>
2017-09-15 16:38:04 +0200

    change test

--- test/test.js
+++ test/test.js
@@ -1,11 +1,8 @@
 var wm = require('./../index.js')
 var options =
   {
-    whileControl: (oldPage, newPage) => {
-      return oldPage !== newPage
-    },
-    lapse: 2000,
-    percentageDiff: 0.1 // if whileControl exist this will not use
+    lapse: 1000,
+    percentageDiff: 0.004 // if whileControl exist this will not use
   }
 var wp = wm.monitor('https://www.google.com/', options).start().start()
 wp.on('start', (url) => console.log(`monitoring of '${url}' start`))
