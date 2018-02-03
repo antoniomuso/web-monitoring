869fab2c0f3760e58a693b32e2fda34fd5492c7f
AntonioMusolino <antoniomusolino007@gmail.com>
2018-02-03 12:52:32 +0100

    removed from control html attribute

--- bin/lib/test_percentage.js
+++ bin/lib/test_percentage.js
@@ -1,6 +1,11 @@
 const request = require('request-promise-native')
 const sanitizeHtml = require('sanitize-html')
 const leven = require('node_leven_distance')
+var sanOpt= {
+  allowedAttributes: {
+  }
+}
+
 
 module.exports = async function (nTest, uri) {
   if (nTest <= 0) throw new Error('nTest must be >0')
@@ -12,7 +17,7 @@ module.exports = async function (nTest, uri) {
   }
   let page
   await request(opt).then((pageBody) => {
-    page = sanitizeHtml(pageBody)
+    page = sanitizeHtml(pageBody,sanOpt)
     // console.log(page.length)
   }).catch((err) => {
     throw err
@@ -22,7 +27,7 @@ module.exports = async function (nTest, uri) {
 
   for (let i = 0; i < nTest; i++) {
     await request(opt).then((pageBody) => {
-      let clear = sanitizeHtml(pageBody)
+      let clear = sanitizeHtml(pageBody,sanOpt)
       percentage = Math.max(percentage, (leven(page, clear) / page.length))
       page = clear
       console.log(`percentage: ${percentage} nTest: ${i}`)
